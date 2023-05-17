b5c27ef3e2711411b042ea9d4489128b1361b877
Lindo Gamede <lindohgamede@gmail.com>
2023-05-17 15:19:52 +0200

    Added Email Service methods for account verifications

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -6,6 +6,7 @@ namespace TalaGrid.Services
 {
     public class EmailService
     {
+        #region Email Service Properties
         // create SmtpClient as client and specify server, port, user name and password
         SmtpClient client = new SmtpClient("smtp-mail.outlook.com", 587, "lindohgamede@outlook.com", "#PeterS23");
 
@@ -15,6 +16,16 @@ namespace TalaGrid.Services
         string Message;
         MailMessage mailMessage;
 
+        #endregion
+
+        #region Email Service Methods
+        /// <summary>
+        /// Send a One Time Pin (OTP) to the user for verifation before the password reset is allowed
+        /// </summary>
+        /// <param name="ToEmail">Recepient Email</param>
+        /// <param name="ToFirstName">Recepient Name</param>
+        /// <param name="ToLastName">Recepient Lastname</param>
+        /// <param name="OTP">One Time Pin</param>
         public void SendOTP(string ToEmail, string ToFirstName, string ToLastName, string OTP)
         {
             Subject = "Account Verification OTP";
@@ -24,13 +35,72 @@ namespace TalaGrid.Services
             client.Send(mailMessage);
         }
 
-        public void Send_GW_Verification(string ToEmail, string ToFirstName, string ToLastName, string OTP)
+        /// <summary>
+        /// Send a verification email to the Admin for GW_Admin account verification
+        /// </summary>
+        /// <param name="ToEmail">Recepient Email</param>
+        /// <param name="ToFirstName">Recepient Name</param>
+        /// <param name="ToLastName">Recepient Lastname</param>
+        /// <param name="IdNumber">Recepient Id Number</param>
+        public void Send_GW_Verification(string ToEmail, string ToFirstName, string ToLastName, string IdNumber)
         {
             Subject = "Admin Verification";
-            Message = $"Hi {ToFirstName} {ToLastName},\n\nPlease see your TalaGrid OTP: {OTP}";
+            Message = $"Hi Admin, \n\nA verification for a new registered GreenWay Africa Admin, {ToFirstName} {ToLastName} " +
+                $"with Id Number: {IdNumber} is required. Please Login into the App and action the request. \n\n\nRegards, \nTalagrid";
             mailMessage = new MailMessage(FromEmail, ToEmail, Subject, Message);
 
             client.Send(mailMessage);
         }
+
+        /// <summary>
+        /// Send a verification response email to the GW_Admin
+        /// </summary>
+        /// <param name="ToEmail">Recepient Email</param>
+        /// <param name="ToFirstName">Recepient Name</param>
+        /// <param name="ToLastName">Recepient Lastname</param>
+        public void Send_GW_Ver_Response(string ToEmail, string ToFirstName, string ToLastName)
+        {
+            Subject = "Admin Verification";
+            Message = $"Hi {ToFirstName} {ToLastName},\n\nPlease note that your account has been verified, you can now Login and use the application." +
+                $"\n\n\nRegards,\nTalagrid Admin";
+            mailMessage = new MailMessage(FromEmail, ToEmail, Subject, Message);
+
+            client.Send(mailMessage);
+        }
+
+        /// <summary>
+        /// Send a verification email to the Admin for BBC_Admin account verification
+        /// </summary>
+        /// <param name="ToEmail">Recepient Email</param>
+        /// <param name="ToFirstName">Recepient Name</param>
+        /// <param name="ToLastName">Recepient Lastname</param>
+        /// <param name="IdNumber">Recepient Id Number</param>
+        public void Send_BBC_Verification(string ToEmail, string ToFirstName, string ToLastName, string IdNumber)
+        {
+            Subject = "Admin Verification";
+            Message = $"Hi Admin, \n\nA verification for a new registered Buy-Back-Center Admin, {ToFirstName} {ToLastName} " +
+                $"with Id Number: {IdNumber} is required. Please Login into the App and action the request. \n\n\nRegards, \nTalagrid";
+            mailMessage = new MailMessage(FromEmail, ToEmail, Subject, Message);
+
+            client.Send(mailMessage);
+        }
+
+        /// <summary>
+        /// Send a verification response email to the BBC_Admin
+        /// </summary>
+        /// <param name="ToEmail">Recepient Email</param>
+        /// <param name="ToFirstName">Recepient Name</param>
+        /// <param name="ToLastName">Recepient Lastname</param>
+        public void Send_BBC_Ver_Response(string ToEmail, string ToFirstName, string ToLastName)
+        {
+            Subject = "Admin Verification";
+            Message = $"Hi {ToFirstName} {ToLastName},\n\nPlease note that your account has been verified, you can now Login and use the application." +
+                $"\n\n\nRegards,\nGreenWay Africa Admin";
+            mailMessage = new MailMessage(FromEmail, ToEmail, Subject, Message);
+
+            client.Send(mailMessage);
+        }
+
+        #endregion
     }
 }
